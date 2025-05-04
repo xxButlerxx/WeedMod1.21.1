d16cfb16c9c25db03f56ebdcebb53caf1ee148fa
Alexander Butler <alexbutler99@yahoo.dk>
2025-05-04 18:33:31 +0200

    Initial commit

---
+++ src/main/java/net/alex/weedmod/WeedMod.java
@@ -0,0 +1,18 @@
+package net.alex.weedmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WeedMod implements ModInitializer {
+	public static final String MOD_ID = "weedmod";
+
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/alex/weedmod/WeedModClient.java
@@ -0,0 +1,10 @@
+package net.alex.weedmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WeedModClient  implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

fa885ff9a91c0ecadba52bbcb4a8d0e40de02c27
xxButlerxx <ukulelejoergen@gmail.com>
2025-05-05 10:54:10 +0200

    Coins and ItemGroups

--- src/main/java/net/alex/weedmod/block/ModBlocks.java
+++ src/main/java/net/alex/weedmod/block/ModBlocks.java
@@ -20,7 +20,7 @@ public class ModBlocks {
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
         return Registry.register(Registries.BLOCK, Identifier.of(WeedMod.MOD_ID,name), block);
-    };
+    }
 
 
     private static void registerBlockItem(String name, Block block) {
